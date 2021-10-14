49493eaaaa0d141dcbe36facd9650e9445ff27dd
kaizhina <goutaigoul@gmail.com>
2021-10-14 13:12:26 +0800

    Seach

--- js/commen.js
+++ js/commen.js
@@ -22,6 +22,8 @@ searchBtn.onclick = function() {
         }
         // 服务器yunyin
         window.location.replace(window.location.protocol + `/yunyin/search/search.html?searchData=${searchInp.value}`)
+            //window.location.replace(https://kaihzina.github.io/yunyin/search/search.html?searchData=${searchInp.value}`)
+        window.location.replace(window.location.protocol + `kaihzina.github.io/yunyin/search/search.html?searchData=${searchInp.value}`)
     }
     // 回车搜索
 searchInp.onfocus = function() {
